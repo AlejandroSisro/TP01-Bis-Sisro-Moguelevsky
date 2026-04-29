87850546e8128a39e4ad19d1c57769a3eef332f0
50233806-create <50233806@est.ort.edu.ar>
2026-04-29 08:19:56 -0300

    Replace integrantes with cabañas in Complejo class

--- Models/Complejo.cs
+++ Models/Complejo.cs
@@ -3,18 +3,18 @@
  {
  public class Complejo
     {
-            private Dictionary <int, Complejo> integrantes = new Dictionary <int, Complejo>();
+            private Dictionary <int, Complejo> cabañas = new Dictionary <int, Complejo>();
     
         public Complejo()
         {
-      Dictionary <int, Complejo> integrantes = new Dictionary <int, Complejo>();
+      Dictionary <int, Complejo> cabañas = new Dictionary <int, Complejo>();
         } 
         private void CargaDatosManual()
         {
-            Complejo Complejo1 = new Complejo(1, "Alejandro", "La mejor cabaña de todas con los colchones mas costosos de la actualidad",4, "Ale.jpg");
-            Complejo Complejo1 = new Complejo(2, "sisro", "un destino inpecable con una increible vista de los alpes italianos ",2, "alpes.jpg");
-            integrantes.Add(Alumno1);
-            integrantes.Add(Alumno2);
+            Cabaña Cabaña1 = new Cabaña(1, "Alejandro", "La mejor cabaña de todas con los colchones mas costosos de la actualidad",4, "Ale.jpg");
+            Cabaña Cabaña2 = new Cabaña(2, "sisro", "Un destino inpecable con una increible vista de los alpes italianos ",2, "alpes.jpg");
+            cabañas.Add(Cabaña1);
+            cabañas.Add(Cabaña2);
         }
     }
    
