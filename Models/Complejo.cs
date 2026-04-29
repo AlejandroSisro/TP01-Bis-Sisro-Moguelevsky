70486f156c0cd3d9f5425c18aea21fc7890ebe92
50233806-create <50233806@est.ort.edu.ar>
2026-04-29 08:24:55 -0300

    Rename CargaDatosManual to CargaDatos and update cabins

--- Models/Complejo.cs
+++ Models/Complejo.cs
@@ -9,12 +9,14 @@
         {
       Dictionary <int, Complejo> cabañas = new Dictionary <int, Complejo>();
         } 
-        private void CargaDatosManual()
+        private void CargaDatos()
         {
-            Cabaña Cabaña1 = new Cabaña(1, "Alejandro", "La mejor cabaña de todas con los colchones mas costosos de la actualidad",4, "Ale.jpg");
-            Cabaña Cabaña2 = new Cabaña(2, "sisro", "Un destino inpecable con una increible vista de los alpes italianos ",2, "alpes.jpg");
+            Cabaña Cabaña1 = new Cabaña(1, "Francia", "La mejor cabaña de todas con los colchones mas costosos de la actualidad",4, "francia.jpg");
+            Cabaña Cabaña2 = new Cabaña(2, "Italia", "Un destino inpecable con una increible vista de los alpes italianos ",2, "alpes.jpg");
+            Cabaña Cabaña3 = new Cabaña(3, "Argentina", "Una cabaña con una vista imperdible del rio de la plata",6, "argentina.jpg");
             cabañas.Add(Cabaña1);
             cabañas.Add(Cabaña2);
+            cabañas.Add(Cabaña3);
         }
     }
    
